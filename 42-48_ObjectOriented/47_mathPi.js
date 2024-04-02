b18d25f89cc829fff6d209769fee68ee64a1e20a
Mohammad Affan <mohammedaffan2305@gmail.com>
2024-04-02 11:23:06 +0530

    revised 'define property' and 'getOwnPropertyDescriptor'

--- 42-48_ObjectOriented/47_mathPi.js
+++ 42-48_ObjectOriented/47_mathPi.js
@@ -1,25 +1,32 @@
 const val = Object.getOwnPropertyDescriptor(Math, 'PI')
-
+//console.log(val);
+/* {
+   value: 3.141592653589793,
+   writable: false,
+   enumerable: false,
+   configurable: false
+ } */
 const chai = {
     name: 'ginger chai',
     price: 250,
     isAvailable: true,
 
-    orderChai: function(){
+    orderChai: function () {
         console.log('code phat giya hai');
     }
 }
-console.log(Object.getOwnPropertyDescriptor(chai, 'name'));
 
-Object.defineProperty(chai, 'name',{
-    // writable: false,
+Object.defineProperty(chai, 'name', {
+    writable: false,
     enumerable: false
 })
+// chai.name = 'chai' //? doesn't take any action. As "writable" property set as "false"
+//* console.log(Object.getOwnPropertyDescriptor(chai, 'name'));
 
 for (const [key, value] of Object.entries(chai)) {
-if(typeof value !== 'function'){
-    console.log(`${key}: ${value}`);
+    if (typeof value !== 'function') {
+        console.log(`${key}: ${value}`);
 
-}
-    // console.log(`${key}: ${value}`); 
+    }
+    console.log(`${key}: ${value}`);
 }
--- zzzPractice.js
+++ zzzPractice.js
@@ -1,7 +1,3 @@
-function abcd(val1, val2, val3) {
-    console.log(this,val1, val2, val3);
-}
-var obj = {age: 19}
-abcd.apply(obj, [1,2,3])
-// var bindedfn = abcd.bind(obj)
-// bindedfn()
+/* let c = { greeting: "Hey!" }
+let d; d = c; console.log(d); c.greeting = 'hello'; console.log(d.greeting); */
+
